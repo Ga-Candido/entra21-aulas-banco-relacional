bc2962492a1f195444f45562b3c8abd6c7e3a665
Ga-Candido <gabrielsousa3002@gmail.com>
2022-06-22 22:19:51 -0300

    Aula01 de Banco de Dados

---
+++ Aula_01/Inserts.sql
@@ -0,0 +1,18 @@
+
+
+
+
+
+insert into cliente (key,nome,cpf) values(1,"Gabriel","0000589");
+insert into cliente (key,nome,cpf) values(2,"Gabriela","005600541");
+insert into cliente (key,nome,cpf) values(3,"Rafaela","08940541");
+insert into cliente (key,nome,cpf) values(4,"Junior","1250541");
+
+
+
+
+
+
+
+
+select * from cliente;
